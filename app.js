6e2ac1c228e82f66fc2444d423d592b1454ea95a
bangadam <bangadam.dev@gmail.com>
2017-09-18 20:14:20 +0700

    updated

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use('/blog/post-detail/:id/comment', commentRoutes);
 
 
 // listen port
-app.listen(3000, function() {
-  console.log('server listening on port 3000');
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log('server is running');
 });
